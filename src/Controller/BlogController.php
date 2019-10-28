e1bfbf9214ec9a1761c2d73c9b208ff4d7745563
xayokozawa <r.yokozawa@xearts.jp>
2019-10-28 15:54:03 +0900

    投稿ページの投稿機能追加
    /create-post
    データベースにデータを追加する機能を追加した。
    まだ機能のみなので、このページに移動するとデバッグ用のデータが自動でデータベースに保存される。

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class BlogController extends AbstractController
 {
@@ -12,8 +16,43 @@ class BlogController extends AbstractController
      */
     public function index()
     {
+        //全てのPOSTを取得する。
+        $posts = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'posts' => $posts
         ]);
     }
+
+    /**
+     * @Route("/create-post", name="create_post")
+     */
+    public function createPost():Response
+    {
+        //$this->getDoctrine()を通してEntityManagerを取得。
+        //Doctrineを介してデータベースにオブジェクトを保存したり、データベースからオブジェクトを取得したりする。
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $newPost = new Post();
+        $newPost -> setTitle('ブログはじめました。');
+        $newPost -> setContent('日々の出来事や趣味などを投稿していきます。');
+        $newPost -> setCreatedAt(new \DateTime());
+
+        //newPostを保存するためにDoctrineと対話する。
+        //newPostを管理下に置く。クエリは作成されない。
+        $entityManager->persist($newPost);
+
+        //INSERTクエリを実行
+        //newPostオブジェクトのデータはデータベースに存在しないため、entityManagerはINSERTクエリを実行し、テーブルに新しい行を追加する。
+        $entityManager->flush();
+
+        return new Response('保存しました。');
+
+
+    }
+
+
 }
